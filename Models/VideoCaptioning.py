19fa4cf45deebe56a4732609ad2590dc20dfe489
Harsh Singh <HarshSingh-01@users.noreply.github.com>
2021-01-27 16:20:35 +0530

    Video Captioning with Flickr8k model

--- Models/VideoCaptioning.py
+++ Models/VideoCaptioning.py
@@ -1,7 +1,6 @@
-
 import time
 import cv2
-
+import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.applications.xception import Xception
@@ -13,7 +12,6 @@ import matplotlib.pyplot as plt
 import argparse
 from pathlib import Path
 from IPython.display import display
-from nltk.translate.bleu_score import corpus_bleu
 
 def startCamera(skip_seconds):
     cameraStream = cv2.VideoCapture(0)
@@ -21,8 +19,10 @@ def startCamera(skip_seconds):
     # Get frame information
     success, frame = cameraStream.read()
     height, width, layers = frame.shape
-    new_h = round(height / 2)
-    new_w = round(width / 2)
+    #new_h = round(height / 2)
+    #new_w = round(width / 2)
+    new_h = 299
+    new_w = 299
 
     # Get frames per seconds.
     fps = int(cameraStream.get(cv2.CAP_PROP_FPS))
@@ -37,17 +37,18 @@ def startCamera(skip_seconds):
         success, frame = cameraStream.read()
         if success == True:
             resized_frame = cv2.resize(frame, (new_w, new_h))
+            #resized_frame = np.resize(frame, (new_w, new_h))
 
             # @Harsh uncomment below line and add your 'predict_caption()' method here.
             # caption_text = model.predict_caption(resized_frame)
 
-            def extract_features(filename, model):
+            def extract_features(file, model):
                 try:
-                    image = Image.open(filename)
+                    image = file                
                 except:
-                    print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
-                image = image.resize((299,299))
-                image = np.array(image)
+                    print("ERROR: Couldn't open image!")
+                #image = image.resize((299,299))
+                #image = np.array(image)
                 # for images that has 4 channels, we convert them into 3 channels
                 if image.shape[2] == 4:
                     image = image[..., :3]
@@ -78,15 +79,14 @@ def startCamera(skip_seconds):
                         break
                 return in_text
 
-            path = Path("/some_path")
-            image_label = '262963190_a78b799e89.jpg'
-            img_path = path / image_label
+            
             max_length = 32
             tokenizer = load(open("Flickr8k/tokenizer.p","rb"))
             model = load_model("Flickr8k/model_9.h5")
+            
             xception_model = Xception(include_top=False, pooling="avg")
-            photo = extract_features(img_path, xception_model)
-            img = Image.open(img_path)
+            photo = extract_features(resized_frame, xception_model)
+            #img = Image.open(img_path)
 
             description = generate_desc(model, tokenizer, photo, max_length)
             print(description)
@@ -95,8 +95,8 @@ def startCamera(skip_seconds):
 
             #print(f'{caption_text}') # later 'caption_text' will be converted into audio.
 
-            # Commented, because hour task is to generate caption (and later convert it into audio)
-            cv2.imshow('Video', frame)
+            # Commented, because our task is to generate caption (and later convert it into audio)
+            cv2.imshow('frame', frame)
 
             if cv2.waitKey(fps) & 0xFF == ord('q'):
                 print('Key \'Q\' is pressed !')
@@ -117,7 +117,7 @@ def startCamera(skip_seconds):
 
 def runApp():
     print('Application started !')
-    startCamera(0)
+    startCamera(3)
 
 def main():
     runApp()
