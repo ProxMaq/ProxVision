002297c34b0d19d7ee1fbbe238d9ccb541545ea3
Harsh Singh <HarshSingh-01@users.noreply.github.com>
2021-04-12 20:00:06 +0530

    video captioning done

--- Models/VideoCaptioning-MSCOCO.py
+++ Models/VideoCaptioning-MSCOCO.py
@@ -18,25 +18,32 @@ from playsound import playsound
 
 
 class Attention(tf.keras.Model):
-    def __init(self, units):
+    def __init__(self, units):
         super(Attention, self).__init__()
         self.W1 = tf.keras.layers.Dense(units)
         self.W2 = tf.keras.layers.Dense(units)
         self.V = tf.keras.layers.Dense(1)
 
-    def Call(self, features, hidden):
+    def call(self, features, hidden):
+        # features(CNN_encoder output) shape == (batch_size, 64, embedding_dim)
 
+        # hidden shape == (batch_size, hidden_size)
+        # hidden_with_time_axis shape == (batch_size, 1, hidden_size)
         hidden_with_time_axis = tf.expand_dims(hidden, 1)
 
-        attention_hidden_layer = (tf.nn.tanh(
-            self.W1(features)+self.W2(hidden_with_time_axis)))
+        # attention_hidden_layer shape == (batch_size, 64, units)
+        attention_hidden_layer = (tf.nn.tanh(self.W1(features) +
+                                             self.W2(hidden_with_time_axis)))
 
+        # score shape == (batch_size, 64, 1)
+        # This gives you an unnormalized score for each image feature.
         score = self.V(attention_hidden_layer)
 
+        # attention_weights shape == (batch_size, 64, 1)
         attention_weights = tf.nn.softmax(score, axis=1)
 
+        # context_vector shape after sum == (batch_size, hidden_size)
         context_vector = attention_weights * features
-
         context_vector = tf.reduce_sum(context_vector, axis=1)
 
         return context_vector, attention_weights
@@ -45,7 +52,7 @@ class Attention(tf.keras.Model):
 class CNN_Encoder(tf.keras.Model):
     def __init__(self, embedding_dim):
         super(CNN_Encoder, self).__init__()
-
+        # shape after fc == (batch_size, 64, embedding_dim)
         self.fc = tf.keras.layers.Dense(embedding_dim)
 
     def call(self, x):
@@ -55,31 +62,55 @@ class CNN_Encoder(tf.keras.Model):
 
 
 class RNN_Decoder(tf.keras.Model):
+
+    # forward_layer = tf.keras.layers.LSTM(self.units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+    # backward_layer = tf.keras.layers.LSTM(self.units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform', go_backwards=True)
+
     def __init__(self, embedding_dim, units, vocab_size):
         super(RNN_Decoder, self).__init__()
         self.units = units
 
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
 
-        self.gru = tf.keras.layers.GRU(
-            self.units, return_sequence=True, return_state=True, recurrent_initalizer='glorot_unitform')
+        self.gru = tf.keras.layers.GRU(self.units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+
+        # self.lstm = tf.keras.layers.LSTM(self.units,
+        #                                    return_sequences=True,
+        #                                    return_state=True,
+        #                                    recurrent_initializer='glorot_uniform')
+        # forward_layer = tf.keras.layers.LSTM(self.units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+        # backward_layer = tf.keras.layers.LSTM(self.units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform', go_backwards=True)
+
+        # self.bidirectional = tf.keras.layers.Bidirectional(forward_layer, backward_layer=backward_layer)
 
         self.fc1 = tf.keras.layers.Dense(self.units)
         self.fc2 = tf.keras.layers.Dense(vocab_size)
 
+        self.attention = Attention(self.units)
+
     def call(self, x, features, hidden):
+        # defining attention as a separate model
         context_vector, attention_weights = self.attention(features, hidden)
 
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
         x = self.embedding(x)
 
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
-        output, state = self.gru(x)
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)  # self.lstm(x) #self.bidirectional
 
+        # shape == (batch_size, max_length, hidden_size)
         x = self.fc1(output)
 
+        # x shape == (batch_size * max_length, hidden_size)
         x = tf.reshape(x, (-1, x.shape[2]))
 
+        # output shape == (batch_size * max_length, vocab)
         x = self.fc2(x)
 
         return x, state, attention_weights
@@ -93,6 +124,7 @@ def load_image(image_path):
     img = tf.image.decode_jpeg(img, channels=3)
     img = tf.image.resize(img, (299, 299))
     img = tf.keras.applications.inception_v3.preprocess_input(img)
+    os.remove(image_path)
     return img, image_path
 
 
@@ -105,8 +137,8 @@ def textToSpeech(text):
     #-------------------------------------------#
 
 
-with open('') as handle:
-    tokenizer = load(handle)
+with open('C:\\Users\Harsh\Desktop\Projects\ProxVision\Models\MS COCO/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 
 def evaluate(image):
@@ -125,7 +157,7 @@ def evaluate(image):
     # Checkpoints
     optimizer = tf.keras.optimizers.Adam()
 
-    checkpoint_path = "/content/drive/MyDrive/Image-Captioning/Training-Checkpoints"
+    checkpoint_path = "C:\\Users\Harsh\Desktop\Projects\Models\Training-Checkpoints"
 
     ckpt = tf.train.Checkpoint(
         encoder=encoder, decoder=decoder, optimizer=optimizer)
@@ -202,8 +234,11 @@ def startCamera(skip_seconds):
             #img = Image.open(img_path)
 
             # description = generate_desc(model, tokenizer, photo, max_length)
-            description, attention_plot = evaluate(resized_frame)
+            image = cv2.imwrite('image'+'.jpg', resized_frame)
+            description, attention_plot = evaluate("image.jpg")
+
             description = " ".join(description)
+            description = description.replace("<end>", "")
             print(description)
 
             # Convert text into speech
