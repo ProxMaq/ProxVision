dd613e9e9a613dd1e873734105992d63f04149f2
Aswin R <43998798+aswinr22@users.noreply.github.com>
2021-09-10 10:59:46 +0530

    Update model.py
    updated

--- Detections/Text_detections/model.py
+++ Detections/Text_detections/model.py
@@ -32,7 +32,7 @@ class model_v1:
         self.No_categories=No_categories
 
 
-    def build_model(self,activation,kernel_initializer,No_categories,filters,pool_size):
+    def build_model(self):
         #input shape must change according to data
         input_sh=Input(shape=(96,96,3))
         model=Conv2D(16,(self.filters),
@@ -57,24 +57,23 @@ class model_v1:
         kernel_initializer=self.kernel_initializer,
         activation=self.activation
         )(model)
-        model=BatchNormalization()(model)
+        #model=BatchNormalization()(model)
         model=MaxPooling2D(pool_size=(self.pool_size)
         )(model)
         #model=BatchNormalization()(model)
 
-        model=TimeDistributed(Flatten())(model)
+        
 
 
         model=LSTM(33,activation='tanh',recurrent_initializer='glorot_uniform')(model)
         model=Dense(1024,activation='relu')(model)
-        model=Dense(1024,activation='relu')(model)
-        model=Dense(self.No_categories,activation='relu')(model)
+        model=Dense(self.No_categories,activation='softmax')(model)
         model = Model(inputs=input_sh, outputs= model)
         return model
 
         
 
 get_model=model_v1(act_funct,ker_init,5,flt,pool)
-mo=get_model.build_model(act_funct,ker_init,5,flt,pool)
+mo=get_model.build_model()
 mo.summary()
     
